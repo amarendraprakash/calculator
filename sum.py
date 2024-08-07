22c58b5cbeef943cfcd7951e217c6556c53af129
amarendraprakash <amarendrahack01@gmail.com>
2024-08-07 20:55:04 +0530

    my first commit

---
+++ sum.py
@@ -0,0 +1,20 @@
+num1=int(input("Enter the number1:"))
+num2=int(input("Enter the number2:"))
+op=input("Enter the +,-,*,/,%,//,** :")
+if op== '+':
+ print(num1+num2)
+elif op== '-':
+ print(num1-num2)
+elif op== '*':
+ print(num1*num2)
+elif op== '/':
+ print(num1/num2)
+elif op=='%':
+ print(num1%num2)
+elif op=='//':
+ print(num1//num2)
+elif op=='**':
+ print(num1**num2)
+else:
+ print("Invalid Operator")
+
